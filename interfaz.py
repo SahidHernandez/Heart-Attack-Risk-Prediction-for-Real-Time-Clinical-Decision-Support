992dee7f77a395d46f5a0e956c5aae2f437579b8
Brandon-nava <a2163010063@alumnos.uat.edu.mx>
2025-06-05 16:10:48 -0600

    Update interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -13,20 +13,20 @@ st.set_page_config(page_title="FIC", page_icon = logo, layout="wide")
 # Cargar configuraciones de modelos
 config_modelos = {
     'Model 1': {
-        'modelo': joblib.load('1erdataset/ensamble_GBC_dataset2.pkl'),
-        'scaler': joblib.load('1erdataset/modelo_standardscaler.pkl'),
-        'variables': ['Age', 'CK-MB', 'Troponin', 'Gender']
+        'model': joblib.load('1st_dataset_final/ensamble_RandomForest_dataset1.pkl'),
+        'scaler': joblib.load('1st_dataset_final/model_standardscaler.pkl'),
+        'variable': ['Age', 'CK-MB', 'Troponin', 'Gender']
     },
     'Model 2': {
-        'modelo': joblib.load('2dodataset/ensamble_GBC_dataset2.pkl'),
-        'scaler': joblib.load('2dodataset/modelo_standardscaler_2.pkl'),
-        'variables': ['exang', 'cp', 'oldpeak', 'thalach', 'ca']
+        'model': joblib.load('2nd_dataset_final/ensamble_DecisionTree_dataset2.pkl'),
+        'scaler': joblib.load('2nd_dataset_final/model_standardscaler.pkl'),
+        'variable': ['exang', 'cp', 'oldpeak', 'thalach', 'ca']
     }
 }
 
 # Inicializar modelo por defecto
-if 'modelo_nombre' not in st.session_state:
-    st.session_state['modelo_nombre'] = 'Model 1'
+if 'model_name' not in st.session_state:
+    st.session_state['model_name'] = 'Model 1'
 
 st.title("Heart Attack Classifier")
 st.write("""
@@ -70,63 +70,63 @@ st.markdown(f"*Selected model:* {modelo_nombre}")
 
 # Obtener modelo, scaler y variables
 config = config_modelos[modelo_nombre]
-modelo = config['modelo']
+model = config['model']
 scaler = config['scaler']
-vars_requeridas = config['variables']
+vars_required = config['variable']
 
 st.markdown("### Enter the patient's data")
 
 # Entradas condicionales
-valores = {}
+values = {}
 
-if 'Age' in vars_requeridas:
+if 'Age' in vars_required:
     valores['Age'] = st.number_input("Age", min_value=0, max_value=120, value=45)
 
-if 'CK-MB' in vars_requeridas:
+if 'CK-MB' in vars_required:
     ckmb = st.number_input("CK-MB", value=2.86, min_value=0.00, format="%.2f")
     valores['CK-MB'] = np.log(ckmb + 1e-10)
 
-if 'Troponin' in vars_requeridas:
+if 'Troponin' in vars_required:
     troponina = st.number_input("Troponin", value=0.003, min_value=0.000, format="%.3f", step=0.001)
     valores['Troponin'] = np.log(troponina + 1e-10)
 
-if 'Gender' in vars_requeridas:
+if 'Gender' in vars_required:
     valores['Gender'] = st.selectbox("Gender", options=["Male", "Female"])
     # Convertir a valor numérico si el modelo lo requiere
     valores['Gender'] = 1 if valores['Gender'] == "Male" else 0
 
-if 'exang' in vars_requeridas:
+if 'exang' in vars_required:
     valores['exang'] = st.selectbox("exang", options=[0, 1])
 
-if 'cp' in vars_requeridas:
+if 'cp' in vars_required:
     valores['cp'] = st.selectbox("cp", options=[0, 1, 2, 3])
 
-if 'oldpeak' in vars_requeridas:
+if 'oldpeak' in vars_required:
     valores['oldpeak'] = st.number_input("oldpeak", value=1.0, format="%.2f")
 
-if 'thalach' in vars_requeridas:
+if 'thalach' in vars_required:
     valores['thalach'] = st.number_input("thalach", min_value=50, max_value=250, value=150)
 
-if 'ca' in vars_requeridas:
+if 'ca' in vars_required:
     valores['ca'] = st.selectbox("ca", options=[0, 1, 2, 3])
 
 # Procesar predicción al hacer clic
 if st.button("Classify"):
     # Asegura el orden y la presencia de todas las variables requeridas
-    entrada = pd.DataFrame([[valores.get(var, np.nan) for var in vars_requeridas]], columns=vars_requeridas)
-    entrada = entrada[scaler.feature_names_in_]
-    entrada_scaled = scaler.transform(entrada)
-    pred = modelo.predict(entrada_scaled)[0]
+    input = pd.DataFrame([[values.get(var, np.nan) for var in vars_required:]], columns=vars_required:)
+    input = entrada[scaler.feature_names_in_]
+    input_scaled = scaler.transform(input)
+    pred = modelo.predict(input_scaled)[0]
 
-    resultado = "Positive" if pred == 1 else "Negative"
+    result = "Positive" if pred == 1 else "Negative"
     color = "red" if pred == 1 else "green"
 
     st.markdown(f"### Result: <span style='color:{color}'>{resultado}</span>", unsafe_allow_html=True)
 
     # Mostrar probabilidades si están disponibles
-    if hasattr(modelo, 'predict_proba'):
-        proba = modelo.predict_proba(entrada_scaled)[0]
-        colores = ['#58b915', "#e97c34"]  # verde y naranja
+    if hasattr(model, 'predict_proba'):
+        proba = model.predict_proba(entrada_scaled)[0]
+        colors = ['#58b915', "#e97c34"]  # verde y naranja
 
         # Usar columnas para centrar la gráfica (3 columnas)
         col1, col2, col3 = st.columns([1, 2, 1])  # col2 es la del centro
@@ -136,7 +136,7 @@ if st.button("Classify"):
                 proba,
                 autopct='%1.1f%%',
                 startangle=90,
-                colors=colores,
+                colors=colors,
                 textprops={'color': 'white', 'weight': 'bold', 'fontsize': 10}
             )
             ax.axis('equal')
@@ -164,12 +164,10 @@ if st.button("Classify"):
 #Pie de página fijo 
 st.markdown("""
     <style>
-    /* Espacio inferior al contenido para que no lo tape el footer */
     .stApp {
         padding-bottom: 80px;
     }
 
-    /* Footer fijo en la parte inferior */
     .footer {
         position: fixed;
         bottom: 0;
