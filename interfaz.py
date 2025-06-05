dc71b1d8920f940f59f476b9a878f7f011fa815b
Brandon-nava <a2163010063@alumnos.uat.edu.mx>
2025-06-05 16:16:44 -0600

    Update interfaz.py

--- interfaz.py
+++ interfaz.py
@@ -12,12 +12,12 @@ st.set_page_config(page_title="FIC", page_icon=logo, layout="wide")
 # Load model configurations
 model_configs = {
     'Model 1': {
-        'model': joblib.load('1st_dataset_final/ensamble_RandomForest_dataset1.pkl'),
+        'model': joblib.load('1st_dataset_final/ensemble_RandomForest_dataset1.pkl'),
         'scaler': joblib.load('1st_dataset_final/model_standardscaler.pkl'),
         'variable': ['Age', 'CK-MB', 'Troponin', 'Gender']
     },
     'Model 2': {
-        'model': joblib.load('2nd_dataset_final/ensamble_DecisionTree_dataset2.pkl'),
+        'model': joblib.load('2nd_dataset_final/ensemble_DecisionTree_dataset2.pkl'),
         'scaler': joblib.load('2nd_dataset_final/model_standardscaler.pkl'),
         'variable': ['exang', 'cp', 'oldpeak', 'thalach', 'ca']
     }
